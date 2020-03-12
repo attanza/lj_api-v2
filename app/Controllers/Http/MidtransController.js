4c5be6dc50af13cf8d61fb9e613c84289f21e4f5
Attanza <myrbx.747@gmail.com>
2020-03-12 15:04:58 +0700

    start midtrans callback debug

--- app/Controllers/Http/MidtransController.js
+++ app/Controllers/Http/MidtransController.js
@@ -6,45 +6,47 @@ const { orderStatus } = use("App/Helpers/Constants")
 class MidtransController {
   async notifHandler({ request, response }) {
     try {
-      const {
-        order_id,
-        status_code,
-        gross_amount,
-        signature_key,
-      } = request.post()
-      // Verify signature
+      // const {
+      //   order_id,
+      //   status_code,
+      //   gross_amount,
+      //   signature_key,
+      // } = request.post()
+      // // Verify signature
 
-      //  and update order Accordingly
-      if (!order_id) {
-        return response.status(400).send(ResponseParser.apiNotFound())
-      }
-      const order = await Order.findBy("order_no", order_id)
-      if (!order) {
-        return response.status(400).send(ResponseParser.apiNotFound())
-      }
-      order.status = orderStatus.COMPLETED
-      await order.save()
-      await order.load("product")
-      // Generate and send activation key
-      const activator = await ProductActivatorTrait.store({
-        order_id: order.id,
-        device_id: order.device_id,
-      })
+      // //  and update order Accordingly
+      // if (!order_id) {
+      //   return response.status(400).send(ResponseParser.apiNotFound())
+      // }
+      // const order = await Order.findBy("order_no", order_id)
+      // if (!order) {
+      //   return response.status(400).send(ResponseParser.apiNotFound())
+      // }
+      // order.status = orderStatus.COMPLETED
+      // await order.save()
+      // await order.load("product")
+      // // Generate and send activation key
+      // const activator = await ProductActivatorTrait.store({
+      //   order_id: order.id,
+      //   device_id: order.device_id,
+      // })
 
-      // Send activation key via email
-      MailHelper.activationCodeMail({
-        order: order.toJSON(),
-        activator: activator.toJSON(),
-      })
+      // // Send activation key via email
+      // MailHelper.activationCodeMail({
+      //   order: order.toJSON(),
+      //   activator: activator.toJSON(),
+      // })
 
-      return response
-        .status(200)
-        .send(
-          ResponseParser.successResponse(
-            activator,
-            "Midtrans notification handler"
-          )
-        )
+      // return response
+      //   .status(200)
+      //   .send(
+      //     ResponseParser.successResponse(
+      //       activator,
+      //       "Midtrans notification handler"
+      //     )
+      //   )
+      console.log(request.post())
+      return "ok"
     } catch (e) {
       console.log("e", e)
       ErrorLog(request, e)

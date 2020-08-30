44a8bcad3f3fe0f104ca5963448bd1180a6500f0
Attanza <myrbx.747@gmail.com>
2020-08-30 07:43:17 +0700

    put error catcher inside task scheduller

--- app/Tasks/DashboardSchedulle.js
+++ app/Tasks/DashboardSchedulle.js
@@ -13,32 +13,39 @@ class DashboardSchedulle extends Task {
   }
 
   async handle() {
-    this.info("Task DashboardSchedulle handle")
+    try {
+      this.info("Task DashboardSchedulle handle")
 
-    const totalMarketings = await User.query()
-      .whereHas("roles", builder => {
-        builder.where("slug", "marketing")
-      })
-      .where("is_active", 1)
-      .count("* as total")
+      const totalMarketings = await User.query()
+        .whereHas("roles", builder => {
+          builder.where("slug", "marketing")
+        })
+        .where("is_active", 1)
+        .count("* as total")
 
-    const totalProducts = await Product.query().count("* as total")
-    const totalUniversities = await University.query().count("* as total")
+      const totalProducts = await Product.query().count("* as total")
+      const totalUniversities = await University.query().count("* as total")
 
-    const dashboardDetails = {
-      total_marketings: totalMarketings[0].total,
-      total_products: totalProducts[0].total,
-      total_universities: totalUniversities[0].total,
+      const dashboardDetails = {
+        total_marketings: totalMarketings[0].total,
+        total_products: totalProducts[0].total,
+        total_universities: totalUniversities[0].total,
+      }
+
+      const whereClause = {
+        id: 1,
+      }
+
+      await Dashboard.findOrCreate(whereClause, dashboardDetails)
+
+      const redisKey = "Dashboard_Data"
+      await RedisHelper.delete(redisKey)
+    } catch (error) {
+      console.log(
+        "Task DashboardSchedulle error: ",
+        JSON.stringify(error, null, 2)
+      )
     }
-
-    const whereClause = {
-      id: 1,
-    }
-
-    await Dashboard.findOrCreate(whereClause, dashboardDetails)
-
-    const redisKey = "Dashboard_Data"
-    await RedisHelper.delete(redisKey)
   }
 }
 
